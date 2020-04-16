b57abaab7f18c971d18c5098e083dc3c8c31e524
juan antonio royo de barrio <juanroyo@MacBook-Pro-de-juan.local>
2020-04-16 13:37:40 +0200

    searcher hecho

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -6,16 +6,31 @@ import {
   Route // for later
 } from 'react-router-dom';
 import { addToCart } from '../actions/cartActions'
-
+import {Form} from 'react-bootstrap'
 
 class Shop extends Component {
-
+  constructor() {
+      super();
+      this.state = {
+        search:null,
+        genre:null
+      };
+    }
   handleClick(_id){
         this.props.addToCart(_id);
       }
+
+      searchSpace=(event)=>{
+   let keyword = event.target.value;
+   this.setState({search:keyword})
+ }
+      searchGenre=(event)=>{
+   let keyword = event.target.value;
+   this.setState({genre:keyword})
+ }
 render(){
 
-
+console.log(this.state.genre)
         /*let itemList = this.props.items.map(item=>{
             return(
 
@@ -42,30 +57,49 @@ render(){
         })*/
         return(
             <div className="container">
+              <Form inline>
+              <Form.Control
+                placeholder="Search"
+                value={this.state.filter}
+                onChange={(e)=>this.searchSpace(e)}
+              />
+            </Form>
+            <div class="form-group">
+              <label for="exampleFormControlSelect1">Example select</label>
+              <select class="form-control" id="exampleFormControlSelect1" onChange={(e)=>this.searchGenre(e)}>
+                <option>select an option</option>
+                <option>House</option>
+                <option>Disco</option>
+                <option>Electronic</option>
+                <option>Rock</option>
+                <option>Techno</option>
+              </select>
+            </div>
                 <h3 className="center">Our items</h3>
                 <div className="box">
-                  {this.props.items.map(item=>{
+                  {this.props.items
+                    .filter((item)=>{
 
+                        if(this.state.search == null && this.state.genre == null) {
+                            return item
+                        } else if (Object.keys(item).some(key => typeof item[key] === "string" && item[key].toLowerCase().includes(this.state.search)) || Object.keys(item).some(key => typeof item[key] === "string" && item[key].toLowerCase().includes(this.state.genre))){
+                             return item
+                        }
+                      })
+                    .map(item=>{
                       return(
-
                           <div className="card" key={item._id}>
-
-
                                   <div className="card-image">
                                        <Link to={`shop/${item._id}`}><img src={item.img}/></Link>
-
                                       <span className="card-title">{item.title}</span>
-
                                       <span to="/" className="btn-floating halfway-fab waves-effect waves-light red" onClick={()=>{this.handleClick(item._id)}}><i className="material-icons">add</i></span>
                                   </div>
-
                                   <div className="card-content">
                                       <p>{item.author}</p>
                                       <p>{item.genre}</p>
                                       <p>{item.desc}</p>
                                       <p><b>Price: {item.price}€</b></p>
                                   </div>
-
                            </div>
                       )
                   })}
